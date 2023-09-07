782dc53c7be0a6c04943b65cf877d7c4e427bd03
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-09-07 19:40:03 +0200

    feat: selection sort

---
+++ algorithms-data-structure/src/main/java/org/micheleverriello/sorting/SelectionSort.java
@@ -0,0 +1,21 @@
+package org.micheleverriello.sorting;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] array) {
+
+        int size = array.length;
+
+        for (int i = 0; i < size - 1; i++) {
+            for(int j = i + 1; j < size; j ++) {
+                if(array[j] < array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
---
+++ algorithms-data-structure/src/test/java/org/micheleverriello/sorting/SelectionSortTest.java
@@ -0,0 +1,25 @@
+package org.micheleverriello.sorting;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SelectionSortTest {
+
+    static int[] array;
+
+    @BeforeAll
+    static void setup() {
+        //filling up array
+        array = new int[]{4, 7, 4, 22, 23, -1, -22, 100};
+    }
+
+    @Test
+    public void selectionSort() {
+        int[] result = SelectionSort.selectionSort(array);
+        int[] expectedResult = new int[]{-22, -1, 4, 4, 7, 22, 23, 100};
+
+        assertArrayEquals(expectedResult, result);
+    }
+}
